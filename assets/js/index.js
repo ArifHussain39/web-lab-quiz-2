225dde3b9d09c6b57fd01824257726a1a0a423f9
ArifHussain39 <arif3337179@gmail.com>
2023-09-27 12:31:00 +0500

    assets folder

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+//task1 
+
+function percentageCalculate(num1, total) {
+    return (num1/total)*100;
+}
+
+let percentage = percentageCalculate(881,1000);
+console.log("Total percentage = " + percentage );
+
+//task2 
+
